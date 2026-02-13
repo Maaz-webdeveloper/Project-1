bc137cfa31d7fe1fb5639ac72d9b4d04e1d8d3ea
Muhammad <maazrajpoot991@gmail.com>
2026-02-13 16:13:20 +0500

    Updated process section layout and scripts

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+const slider = document.getElementById("processSlider");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  // Mouse ki position calculate karna
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.style.cursor = "grabbing";
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // Agar click nahi kiya hua toh kuch na karo
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Scroll ki speed (2x)
+  slider.scrollLeft = scrollLeft - walk;
+});
